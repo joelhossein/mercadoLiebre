73ae2c655be36f9116864ac9fd495e5f7efea1a2
joelhossein <joelhossein@protonmail.com>
2022-03-23 20:31:17 -0300

    se modifica Procfile y app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000, () =>
-    console.log("Servidor levantado en el puerto 3000")
-);
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor levantado en el puerto 3000");
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
