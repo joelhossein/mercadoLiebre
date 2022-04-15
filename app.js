f5b76a09d9c99b6e97d5f95e5abb6f410ecd6fca
joelhossein <joelhossein@protonmail.com>
2022-04-15 17:57:43 -0300

    se incluye express-generator

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const mainRoutes = require('./src/routes/mainRoutes');
-const path = require('path');
 
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', './src/views');  
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
+
+// Routes
 app.use('/', mainRoutes);
 
-app.set('view engine', 'ejs');
-app.set('views', './src/views');
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor levantado en el puerto 3000");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
