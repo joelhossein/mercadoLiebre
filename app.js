9e789b59ddbb98c9db114301478d9fe4963f6c2c
joelhossein <joelhossein@protonmail.com>
2022-04-15 17:43:32 -0300

    se agrega src

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const mainRoutes = require('./routes/mainRoutes');
+const mainRoutes = require('./src/routes/mainRoutes');
 const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 app.use('/', mainRoutes);
 
 app.set('view engine', 'ejs');
+app.set('views', './src/views');
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor levantado en el puerto 3000");
